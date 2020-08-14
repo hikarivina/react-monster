1c6a95e731b1733328ae965f7622e709271138b3
dangnh <dangnguyenhuu88@gmail.com>
2020-08-14 23:41:22 +0900

    Updates

---
+++ precache-manifest.64e7cda633e7cc3c4f2b9c5a5afb0080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1ce9a5a032537a3cb3b94ee27a519a",
+    "url": "/react-monster/index.html"
+  },
+  {
+    "revision": "f467b58299132648863c",
+    "url": "/react-monster/static/css/main.0d13225b.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/react-monster/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f467b58299132648863c",
+    "url": "/react-monster/static/js/main.8dd9ae3c.chunk.js"
+  },
+  {
+    "revision": "b4f452715347b5fba6a9",
+    "url": "/react-monster/static/js/runtime-main.29efa540.js"
+  }
+]);
